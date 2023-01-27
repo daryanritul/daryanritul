e2292c21a095ceee1505ee51e9c6e2c6e89ca205
Ritul Daryan <daryanritul@gmail.com>
2023-01-28 04:54:06 +0530

    Colors/Text Updated

--- src/App.js
+++ src/App.js
@@ -3,20 +3,7 @@ import React from 'react';
 const App = () => {
   return (
     <div>
-      <h1>Hellow</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe itaque
-        velit pariatur nobis sapiente quisquam quas vitae sequi ratione, tempora
-        modi adipisci numquam aliquam! Dicta labore possimus officiis
-        consequuntur temporibus omnis autem repudiandae aliquam dolore, cum
-        harum magni, ab ipsa ipsum ratione earum ad cupiditate eaque inventore
-        vel cumque minus. A molestiae voluptate nihil inventore. Consequuntur,
-        hic. Vitae repudiandae facilis architecto quasi, repellendus est
-        distinctio velit dolor tempora maxime suscipit dolorum impedit sint
-        nostrum a? Velit magni fugit earum officiis nulla blanditiis atque
-        dolorem, repudiandae nobis vitae et praesentium rerum itaque doloremque
-        possimus deserunt, soluta quasi ullam non iste est?
-      </p>
+      <h2>Hellow World</h2>
     </div>
   );
 };
